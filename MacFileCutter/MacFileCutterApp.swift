02c8f4145f1b686a8a04b5a12a95a4988e3f7ccb
digkill <digkill@mail.ru>
2025-07-31 03:12:08 +0500

    Initial Commit

---
+++ MacFileCutter/MacFileCutterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacFileCutterApp.swift
+//  MacFileCutter
+//
+//  Created by Digkill on 31.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct MacFileCutterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
